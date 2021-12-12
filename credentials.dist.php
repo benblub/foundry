a13d5932cf6756eba5ff2f2b53841a2bf23ea134
benjaminknecht <knecht.benjamin@yahoo.de>
2021-12-12 21:50:49 +0100

    work with doctrine migrations in this projects

---
+++ credentials.dist.php
@@ -0,0 +1,14 @@
+<?php
+// rename this file to credentials.php
+// add your database credentials here
+// now you are able to use: php vendor/bin/doctrine-migrations
+
+function getCredentials()
+{
+    return [
+        'dbname' => '',
+        'user' => '',
+        'password' => '',
+        'host' => '',
+    ];
+}
